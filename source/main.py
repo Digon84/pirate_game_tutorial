20747ef08906be53556e9934f2235786e86a53d1
Marcin Rudnicki <m.rudnicki84@gmail.com>
2024-01-29 22:15:13 +0100

    Add initial Game and Overworld

--- source/main.py
+++ source/main.py
@@ -3,11 +3,23 @@ import sys
 from settings import *
 from level import Level
 from game_data import level_0
+from source.overworld import Overworld
+
+
+class Game:
+    def __init__(self):
+        self.max_level = 3
+        self.overworld = Overworld(0, self.max_level, screen)
+
+    def run(self):
+        self.overworld.run()
+
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
-level = Level(level_0, screen)
+# level = Level(level_0, screen)
+game = Game()
 
 while True:
     for event in pygame.event.get():
@@ -16,6 +28,7 @@ while True:
             sys.exit()
 
     screen.fill('black')
-    level.run()
+    # level.run()
+    game.run()
     pygame.display.update()
     clock.tick(60)

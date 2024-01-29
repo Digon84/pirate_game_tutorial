265b0a80b7692e9bbdedf393bfeb28e75af74b11
Marcin Rudnicki <m.rudnicki84@gmail.com>
2024-01-29 22:01:09 +0100

    Remove old map

--- source/settings.py
+++ source/settings.py
@@ -1,20 +1,6 @@
-level_map = [
-    '                            ',
-    '                            ',
-    '                            ',
-    ' XX    XXX            XX    ',
-    ' XX P                       ',
-    ' XXXX         XX         XX ',
-    ' XXXX       XX              ',
-    ' XX    X  XXXX    XX  XX    ',
-    '       X  XXXX    XX  XXX   ',
-    '    XXXX  XXXXXX  XX  XXXX  ',
-    'XXXXXXXX  XXXXXX  XX  XXXX  '
-]
 vertical_tile_number = 11
 tile_size = 64
 
 screen_height = vertical_tile_number * tile_size
 screen_width = 1200
 scroll_x_margin = 200
-screen_height = len(level_map) * tile_size
